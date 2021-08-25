c05d00b38ecce4bae2d92573c065ba715257711c
mdominguez <mocomauricio@gmail.com>
2021-08-25 14:43:32 -0400

    produccion api aiepy db

--- softwarani/settings.py
+++ softwarani/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = 'django-insecure-o@xxe1m=10&13$*o5kq$vvin0b00p=plvn30%xyk8nq-1b^5sq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+PRODUCTION = True
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -85,13 +87,24 @@ WSGI_APPLICATION = 'softwarani.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if(PRODUCTION):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'aiepy',
+            'USER': 'aiepyuser',
+            'PASSWORD': 'db413p1',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
